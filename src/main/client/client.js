191d1ffe0b29f038c316ee36b367af2eafdcf08c
Maria Sankina <mariasankina@Marias-MBP.lan>
2018-03-08 15:45:33 +0300

    added mouseflow

--- src/main/client/client.js
+++ src/main/client/client.js
@@ -5,10 +5,21 @@ import {App} from '../app/app';
 import './client.scss';
 import mixpanel from 'mixpanel-browser';
 
+//mixpanel
 window.mixpanel = mixpanel;
 mixpanel.init('89c5c0bb6af5172d87b01e9e06839b2e');
 
+//yandex metrika
 window.yaCounter = new Ya.Metrika({id: 47973731});
+
+//mouseflow
+window._mfq = window._mfq || [];
+(function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.async = true;
+    mf.src = "//cdn.mouseflow.com/projects/355b66e5-d2a4-4c4b-8a5e-a0f778ba3dae.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+})();
 
 hydrate((
     <BrowserRouter>
